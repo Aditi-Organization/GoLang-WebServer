395ab59a00066fc70eac9385028f4296d4eec6dc
lukosemanuel7 <lukosemanuel7@gmail.com>
2021-04-22 00:59:07 -0400

    project structure

---
+++ review-app/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    pathMatch:'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
